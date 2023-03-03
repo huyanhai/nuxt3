e0e9b7bce7763bd44f51d32f1c24d147ec8418a7
Huyanhai <810839700@qq.com>
2023-03-03 11:50:23 +0800

    feat: 服务请求修改

--- server/api/show/[id].ts
+++ server/api/show/[id].ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
-  const res = await $fetch.raw("http://localhost:3001/health");
+  const res = await $fetch.raw("https://www.baidu.com");
   return {
     params: event.context.params?.id,
     res: res._data,
